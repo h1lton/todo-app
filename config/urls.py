ef5990049bc92b0063a3861e49d63c95598e81b0
Sergei Hilton <uzoversergey@gmail.com>
2023-09-27 23:17:27 +0500

    добавил drf-yasg(swagger) Он документирует api. Правда есть пару вопросов с реализацией документацией сериалайзеров.

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TODO App API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('tasks/', include('tasks.urls')),
